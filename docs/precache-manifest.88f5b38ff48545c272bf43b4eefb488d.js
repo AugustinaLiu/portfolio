5ba6b1dc8f1840ae217c57ba63e82320d1899eaf
Ao Liu <aoliu@Aos-MacBook-Pro.local>
2020-02-12 10:16:49 -0800

    1st commit

---
+++ docs/precache-manifest.88f5b38ff48545c272bf43b4eefb488d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "790f2528d3f2cde99391f28716c6abb5",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "06e06652c38422cea624",
+    "url": "/portfolio/static/css/main.c350ee45.chunk.css"
+  },
+  {
+    "revision": "f686e99a9b6a6c927b70",
+    "url": "/portfolio/static/js/2.4afbc96e.chunk.js"
+  },
+  {
+    "revision": "f86a4e17d3cb58f717d9f416937db9aa",
+    "url": "/portfolio/static/js/2.4afbc96e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06e06652c38422cea624",
+    "url": "/portfolio/static/js/main.645684b1.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "4317be933b8b615a4b7847d40093ae3f",
+    "url": "/portfolio/static/media/header.4317be93.jpg"
+  }
+]);
